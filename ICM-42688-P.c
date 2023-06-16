78b37f2a45ceb2ede50da11e8d8b8994c06e5279
GabrielFS <42684942+GabrielFSs@users.noreply.github.com>
2023-06-16 17:16:37 -0300

    Update ICM-42688-P.c

--- ICM-42688-P.c
+++ ICM-42688-P.c
@@ -1,12 +1,11 @@
 /*
  * ICM-42688-P.c
  *
- *  Created on: Jun 10, 2023
- *      Author: Gabriel Felipe
+ *     Created on: Jun 10, 2023
+ *     Author: Gabriel Felipe
  */
 
 #include "ICM-42688-P.h"
-#include "stm32f4xx_hal_i2c.h"
 #include <stdint.h>
 
 const uint16_t i2c_timeout = 10;
@@ -16,7 +15,7 @@ uint8_t ICM42688_INIT(I2C_HandleTypeDef *I2Cx)
     uint8_t check;
     uint8_t Data;
 
-    HAL_I2C_Mem_Read(I2Cx, ICM42688_ADDRESS, WHO_AM_I, 1, &check, 1, i2c_timeout);
+    HAL_I2C_Mem_Read(I2Cx, ICM42688_ADDRESS, WHO_AM_I_G, 1, &check, 1, i2c_timeout);
 
     if (check == 104)  // Verify if the address is 0x68
         {
@@ -29,7 +28,7 @@ uint8_t ICM42688_INIT(I2C_HandleTypeDef *I2Cx)
             HAL_I2C_Mem_Write(I2Cx, ICM42688_ADDRESS, PWR_MGMT0, 1, &Data, 1, i2c_timeout);
 
             // Set the configuration about the interruption 1 (INT1) generator to MPU, INT_CONFIG 0x14
-            // 0x001 to Active High, 0x010 to set Push Pull, BIT 2 = 0 to set Latched mode
+            // 001 to Active High, 010 to set Push Pull, BIT 2 = 0 to set Latched mode
             Data = 0x3;
             HAL_I2C_Mem_Write(I2Cx, ICM42688_ADDRESS, INT_CONFIG, 1, &Data, 1, i2c_timeout);
 
@@ -39,12 +38,12 @@ uint8_t ICM42688_INIT(I2C_HandleTypeDef *I2Cx)
             HAL_I2C_Mem_Write(I2Cx, ICM42688_ADDRESS, INT_STATUS, 1, &Data, 1, i2c_timeout);
 
             // Set accelerometer configuration to 0x50
-            // ACCEL_FS_SEL = 0x011 -> 2g, ACCEL_ODR = 0110 -> 1kHz (default)
+            // ACCEL_FS_SEL = 0000011 -> 2g, ACCEL_ODR = 0110 -> 1kHz (default)
             Data = 0x66;
             HAL_I2C_Mem_Write(I2Cx, ICM42688_ADDRESS, ACCEL_CONFIG0, 1, &Data, 1, i2c_timeout);
 
             // Set accelerometer configuration to 0x4F
-            // GYRO_FS_SEL = 0x011 -> 250dps, GYRO_ODR = 0110 -> 1kHz (default)
+            // GYRO_FS_SEL = 011 -> 250dps, GYRO_ODR = 0110 -> 1kHz (default)
             Data = 0x66;
             HAL_I2C_Mem_Write(I2Cx, ICM42688_ADDRESS, GYRO_CONFIG0, 1, &Data, 1, i2c_timeout);
             return 0;
@@ -64,7 +63,9 @@ void ICM42688_READ_ACCEL(I2C_HandleTypeDef *I2Cx, ICM42688_t *DataStruct)
     DataStruct->Accel_Y = (int16_t) (Rec_Data[2] << 8 | Rec_Data[3]);
     DataStruct->Accel_Z = (int16_t) (Rec_Data[4] << 8 | Rec_Data[5]);
 
-    //This ICM-42688 does not need to divide the value by a scale
+    DataStruct->Ax = DataStruct->Accel_X/ 16384.0;
+    DataStruct->Ay = DataStruct->Accel_Y/ 16384.0;
+    DataStruct->Az = DataStruct->Accel_Z/ 16384.0;
 }
 
 
@@ -79,6 +80,10 @@ void ICM42688_READ_GYRO(I2C_HandleTypeDef *I2Cx, ICM42688_t *DataStruct)
     DataStruct->Gyro_X = (int16_t) (Rec_Data[0] << 8 | Rec_Data[1]);
     DataStruct->Gyro_Y = (int16_t) (Rec_Data[2] << 8 | Rec_Data[3]);
     DataStruct->Gyro_Z = (int16_t) (Rec_Data[4] << 8 | Rec_Data[5]);
+
+    DataStruct->Gx = DataStruct->Gyro_X/ 131.0;
+    DataStruct->Gy = DataStruct->Gyro_Y/ 131.0;
+    DataStruct->Gz = DataStruct->Gyro_Z / 131.0;
 }
 
 
